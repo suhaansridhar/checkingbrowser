a4fc5efba57474f35c2dc90349b04a454ef7e1ca
suhaan sridhar <suhaansridhar@suhaans-MacBook-Pro.local>
2023-05-22 15:42:17 +0100

    added faq, help and made changes in home components

--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -3,37 +3,37 @@ export default function Faq(){
         <div className="faq--container">
             <h3>Frequently Asked Questions</h3><br />
             <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p>
+                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p><br />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
             </div>
-            <hr />
-            <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
-            </div><hr />
 
             <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p>
+                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p><br />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
-            </div><hr />
+            </div>
 
             <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p>
+                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p><br />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
-            </div><hr />
+            </div>
 
             <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p>
+                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p><br />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
-            </div><hr />
+            </div>
 
             <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p>
+                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p><br />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
-            </div><hr />
+            </div>
 
             <div className="question">
-                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p>
+                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p><br />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, quaerat!</strong></p><br />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores accusamus harum, sit perferendis ab laborum? Mollitia magni excepturi nihil repudiandae provident quas quo expedita aliquam tempore perspiciatis laudantium ipsum reprehenderit obcaecati dolorem, distinctio, laboriosam ut porro velit repellat at eaque libero quidem voluptatibus culpa! Explicabo dolor numquam, rem amet iste harum, modi fugiat ullam perferendis quo libero ad unde expedita asperiores, enim corporis ipsa accusamus mollitia aut maxime quaerat nulla?</p>
             </div>
         </div>
--- src/Components/HelpLayout.js
+++ src/Components/HelpLayout.js
@@ -5,11 +5,11 @@ import Contact from './Contact';
 
 const HelpLayout = () => {
   return (
-    <div>
+    <div className='help--layout--container'>
         <h4>This is a help Section where you can contact us or search for frequently asked question</h4>
         <nav className='help--layout--container--links'>
-            <NavLink to="faq" element={<Faq />}>Faq</NavLink>
-            <NavLink to="Contact" element={<Contact />}>Contact</NavLink>
+            <NavLink to="faq">Faq</NavLink>
+            <NavLink to="Contact">Contact</NavLink>
         </nav>
     <Outlet />
     </div>
